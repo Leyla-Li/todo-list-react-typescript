4fd0deff2d0975365878c7c8553ef18555d8629a
leyla li <leylali@qq.com>
2020-03-19 16:10:17 -0700

    create types file to store todo types and imported it into other files

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
 import React from 'react';
 
+import { TodoListItem } from './TodoListItem';
+import { Todo } from './types';
+
+const todos: Array<Todo> = [
+  {text: 'Cook lunch', complete: true}, 
+  {text: 'Walk Cat', complete: false}
+];
+
 function App() {
   return (
-    <div>Hello World</div>
+    <TodoListItem />
   );
 }
 
---
+++ src/TodoListItem.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Todo } from './types';
+
+interface TodoListItemProps {
+    todo: Todo;
+}
+
+export const TodoListItem: React.FC<TodoListItemProps> = ({todo}) => {
+    return <li>
+                <label>
+                    <input type='checkbox' checked={todo.complete} />
+                </label>
+            </li>;
+};
