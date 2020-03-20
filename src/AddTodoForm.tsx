214d81ba3cac189de440e844fd13d61c72851a70
leyla li <leylali@qq.com>
2020-03-20 15:15:06 -0700

    create a method to handle change of input of adding form

--- src/AddTodoForm.tsx
+++ src/AddTodoForm.tsx
@@ -1,9 +1,15 @@
-import React from 'react';
+import React, { useState, ChangeEvent } from 'react';
 
 export const AddTodoForm: React.FC = () => {
+    const [newTodo, setNewTodo] = useState('');
+
+    const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+        setNewTodo(e.target.value);
+    }
+
     return (
         <form>
-            <input type='text' />
+            <input type='text' value={newTodo} onChange={handleChange} />
             <button type='submit'>Add todo item</button>
         </form>
     );
